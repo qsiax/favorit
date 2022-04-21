38a7772303c392a09355e75c7ab2ec469a4c3e43
Team-Stone <84223696+Team-Stone@users.noreply.github.com>
2022-04-21 17:52:38 +0900

    STONE

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './styles/all.scss'
+import gsap from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+import TweenMax from "gsap/all"
+import { Power2, Elastic, CSSPlugin, Expo } from "gsap/all"
+import Swiper from 'swiper/bundle'
+import 'swiper/css'
+
+gsap.registerPlugin(ScrollTrigger, TweenMax);
